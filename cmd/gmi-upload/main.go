53214ad3bc4fdba690afec392b0f5c5ff35cadd8
Martin Schmitt 🐧 <mas@scsy.de>
2022-12-20 21:21:09 +0100

    xattr operations on file instead of handle

--- cmd/gmi-upload/main.go
+++ cmd/gmi-upload/main.go
@@ -1,4 +1,5 @@
 package main
+
 import "fmt"
 import "os"
 import "errors"
@@ -7,14 +8,15 @@ import "github.com/pkg/xattr"
 import "github.com/akamensky/argparse"
 
 // https://api.getmyinvoices.com/accounts/v3/doc/#tag/Document/operation/Upload%20new%20document
-// xattrs used by this program:
-// - user.putmybills.upload-status (uploading or done)
-// - user.putmybills.documentUid
+
+const StatusAttribute string = "user.de.scsy.putmybills.upload-status"
+const DocUidAttribute string = "user.de.scsy.putmybills.document-uid"
 
 func main() {
 	var err error
 	var UploadStatusBytes []byte
 	var UploadStatus string
+
 	// Establish arguments
 	parser := argparse.NewParser("gmi-upload", "Upload document to the GetMyInvoices API")
 	apitoken := parser.String("a", "apitoken", &argparse.Options{Required: true, Help: "API token"})
@@ -40,12 +42,12 @@ func main() {
 		fmt.Printf("ERROR: File not found: %s\n", *file)
 		os.Exit(1)
 	}
+	fh.Close()
 
 	// Test file for xattr capability
-	XattrList, err := xattr.FList(fh);
+	XattrList, err := xattr.List(*file);
 	if err != nil {
 		fmt.Printf("ERROR: Can't retrieve extended attributes for: %s\n", *file)
-		fh.Close()
 		os.Exit(1)
 	}
 	if true == *verbose {
@@ -55,7 +57,7 @@ func main() {
 
 	// Get upload status xattr from file - user.putmybills.upload-status
 	// error is expected at this point (empty attribute). No error handling.
-	UploadStatusBytes, err = xattr.FGet(fh, "user.putmybills.upload-status");
+	UploadStatusBytes, err = xattr.Get(*file, StatusAttribute);
 	UploadStatus = string(UploadStatusBytes)
 
 	// -> uploading - Previous upload failed without cleanup: error message and exit != 0
@@ -64,13 +66,11 @@ func main() {
 			fmt.Printf("Will resume aborted upload for: %s\n", *file)
 		} else {
 			fmt.Printf("ERROR: Aborted upload detected for: %s (maybe retry using --resume)\n", *file)
-			fh.Close()
 			os.Exit(1)
 		}
 	// -> done - Previous upload succeeded, info message and abort
 	} else if "done" == UploadStatus {
 		fmt.Printf("File already marked as uploaded: %s\n", *file)
-		fh.Close()
 		os.Exit(0)
 	// -> nothing - Proceed
 	} else {
@@ -80,15 +80,13 @@ func main() {
 	}
 
 	// Set upload status xattr: uploading
-	err = xattr.FSet(fh, "user.putmybills.upload-status", []byte("uploading"))
+	err = xattr.Set(*file, StatusAttribute, []byte("uploading"))
 	if err != nil {
 		fmt.Printf("ERROR: Can't set extended attributes for: %s\n", *file)
-		fh.Close()
 		os.Exit(1)
 	}
 
 	// Ready to upload
-	fh.Close()
 
 	// Upload to API
 
